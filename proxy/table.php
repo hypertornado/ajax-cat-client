9cf608f106990237d2a17378cc3f94551ab0a40a
Ondrej Odchazel <hypertornado@gmail.com>
2012-04-23 21:31:01 +0200

    clean new branch

--- proxy/table.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$q = $_GET['q'];
-$q = str_replace("\n", " ", $q);
-$q = htmlspecialchars($q);
-$q = urlencode($q);
-
-echo file_get_contents("http://localhost:8888/table?pair=".$_GET['pair']."&q=".$q);
