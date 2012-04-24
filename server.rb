026e33a35b62f2b77f8db49a82fd6c86c7043fbd
Ondrej Odchazel <hypertornado@gmail.com>
2012-04-24 16:23:13 +0200

    out redirect

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@
 
 require 'sinatra.rb'
 
-pipe = IO.popen("nohup ../ajax-cat-server/ajax-cat-server", "r")
+pipe = IO.popen("nohup ../ajax-cat-server/ajax-cat-server >> log 2>&1 &", "r")
 
 get '/' do
   redirect '/index.html'
